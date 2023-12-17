db38faecadb5697caea0e3474ad507a935c17381
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-12-17 13:48:31 +0300

    Js-Algorithm
    factorial algorithması için 2. çözüm yolu ekledim.

--- 06-faktoriyel.js
+++ 06-faktoriyel.js
@@ -1,6 +1,21 @@
+/*First Factorial
+Using the Javascript language, have the function firstFactorial(num) take the num parameter being passed and return the factorial of it(ie. If num = 4, return (4*3*2*1)). For the cases, the range will be betweeb 1 and 10. 
+*/
 
+//First
+const firstFactorial = (num) => {
+  let factorial = 1 //0 ve 1 de 1 döner
+  for (let i = 2; i <= num; i++) {
+    //console.log(i);
+    factorial *= i
+  }
+  return factorial
+}
 
-function faktoriyel(num) {
+console.log(firstFactorial(8))
+
+//Second
+/* function faktoriyel(num) {
     if(num === 0 || num === 1){
         return 1;
     }else {
@@ -8,4 +23,4 @@ function faktoriyel(num) {
     }
 }
 
-console.log(faktoriyel(4))
+console.log(faktoriyel(8)) */
