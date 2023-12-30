bee0ab447704e9358199fa61dadbb5b456b46d10
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-12-30 18:39:31 +0300

    Js-Algorithm
    Palindrom çözümüne yeni bir yol ekledim.

--- 03-palindrom.js
+++ 03-palindrom.js
@@ -1,6 +1,20 @@
+/* Palindrome
+Using the Javascript language, have the function Palindrome(str) take the parameter being passed and retuen the string true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return the string false.
+
+For example: "racecar" is also "racecar" backwards.
+--Punctuation and numbers will not be part of the string.
+*/
+
+const isPalindrome = (str) => {
+  let newStr = str.replace(/ /g, '').toLowerCase()
+  return newStr.split('').reverse().join('') === newStr ? true : false
+}
+
+console.log(isPalindrome('racecar'))
+
 //Palindrom : okunuşu ile tersten okunuşu aynı olan sözcüklere denir. kepek,küçük,kazak,mum vb.
 
-function Palindrom(text) {
+/* function Palindrom(text) {
      let splitted = text.split("")
      let reversed = text.split("").reverse()
 
@@ -11,4 +25,4 @@ function Palindrom(text) {
      }
 }
 
-Palindrom("kazak")
+Palindrom("kazak") */
